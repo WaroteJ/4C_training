dec2e3deb018f54c49c383b1309d83f2ef4d0d72
Jerome <jerome.pihet@viacesi.fr>
2019-10-18 21:49:20 +0200

    Code cleaning

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-  // Add a new <li> element
 $(function(){
-    $(".add").click(function(){
+    $(".add").click(function(){ // Addition of a new element
         let value = $("#newValue").val();
         if (value!==''){    
-            spanAU="<div class =\"arrows\"><span class=\"arrowUp\">\u2191</span>";  //span ArrowUp
-            spanAD="<span class=\"arrowDown\">\u2193</span></div>";                 //span ArrowDown
+            spanAU="<div class =\"arrows\"><span class=\"arrowUp\">\u2191</span>";  //span ArrowUp + div opening
+            spanAD="<span class=\"arrowDown\">\u2193</span></div>";                 //span ArrowDown + div closing
             spanC="<span class=\"close\">\u00D7</span>"                             //span Close
             $("ul#myList").append("<li><p>"+value+"</p>"+spanAU+spanAD+spanC+"</li>");//Append our new element to the list
         }
@@ -13,31 +12,31 @@ $(function(){
         $("#newValue").val(''); //Empty the input field
     })
     $("#newValue").keypress(function(e){
-        if(e.which==13){    // enter ASCII code 
+        if(e.which==13)    // enter key ASCII code 
             $(".add").click();
-        }
     });
 
-    $("#myList").click(function(e){ //Click on an <li> element or button included in one
-        $item=e.target;
-        if ($item.tagName==="LI"){  // toggle the "checked" class
-            $item.classList.toggle("checked");
+
+    $("#myList").click(function(e){ //Move or delete a <li> depending on the clicked area
+        let item=e.target;
+        if (item.tagName==="LI"){  // toggle the "checked" class on <li>
+            item.classList.toggle("checked");
         }
-        else if ($item.parentNode.tagName==="LI"){
-            $item.parentNode.classList.toggle("checked");
+        else if (item.parentNode.tagName==="LI" && item.tagName==="P"){ // handle a click on a <p> child
+            item.parentNode.classList.toggle("checked");
         }
-        switch ($item.className) {
+        switch (item.className) {
             case "close":                           // click = close button => remove the <li>
-                    $item.parentNode.remove();
+                    item.parentNode.remove();
                 break;
             case "arrowUp":                  // click = up arrow => move up the <li>
-                    $item.parentNode.parentNode.classList.toggle("toMove");    //We use classes to recognise our elements to move
-                    $(".toMove").prev("li").toggleClass("moveUp");  // inside the list
+                    item.parentNode.parentNode.classList.toggle("toMove");    //We use classes to recognise our elements to move
+                    $(".toMove").prev("li").toggleClass("moveUp");             //inside the list
                     $(".toMove").insertBefore($(".moveUp"));
                     $("li").removeClass("toMove moveUp");
                 break;
             case "arrowDown":                // click = down arrow => move down the <li>
-                    $item.parentNode.parentNode.classList.toggle("toMove");
+                    item.parentNode.parentNode.classList.toggle("toMove");
                     $(".toMove").next("li").toggleClass("moveDown");               
                     $(".toMove").insertAfter($(".moveDown"));
                     $("li").removeClass("toMove moveDown");
@@ -47,8 +46,9 @@ $(function(){
         }
     })
 
+
     spanAU="<div class =\"arrows\"><span class=\"arrowUp\">\u2191</span>";  
     spanAD="<span class=\"arrowDown\">\u2193</span></div>";
     spanC="<span class=\"close\">\u00D7</span>"             
-    $("li").append(spanAU+spanAD+spanC);//Append our buttons to every existing <li>
+    $("li").append(spanAU+spanAD+spanC);    //Append our buttons to every existing <li>
 });
